4e770295488c71f890674e31ae77cc6ae9aec831
Ivan Iglesias <iiglesiasbilbao@gmail.com>
2022-12-31 17:25:30 +0100

    1.6 Component Props

---
+++ 1.6 Component Props/js/components/AppButton.js
@@ -0,0 +1,31 @@
+export default {
+
+    // when setting class bindings, we can use strings, arrays or objects
+
+    template: `
+        <button
+            :class="{
+                'border rounded px-5 py-2 disabled:cursor-not-allow': true,
+                'bg-blue-600 hover:bg-blue-700': type === 'primary',
+                'bg-purple-200 hover:bg-purple-400': type === 'secondary',
+                'bg-gray-200': type === 'muted',
+                'is-loading': processing
+            }"
+            :disabled="processing"
+        >
+            <slot/>
+        </button>
+    `,
+
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+
+        processing: {
+            type: Boolean,
+            default: false
+        }
+    },
+};
