8905950e23f68247b91f310de69b452009503437
Ivan Iglesias <iiglesiasbilbao@gmail.com>
2023-01-06 22:12:33 +0100

    1.8 Handle a Form Submission

---
+++ 1.8 Handle a Form Submission/js/components/Assignments.js
@@ -0,0 +1,57 @@
+import AssignmentList from "./AssignmentList.js"
+
+export default {
+    components: { AssignmentList },
+
+    template: `
+        <section class="space-y-6">
+            <assignment-list :assignments="filters.inProgress" title="In Progresss"></assignment-list>
+            <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+
+            <!--
+                v-on:submit == @:submit
+                @:submit.prevent = avoid using 'e.preventDefault();' in the function
+            -->
+            <form @submit.prevent="add">
+                <div class="border border-gray-600 text-black">
+                    <input v-model="newAssignment" placeholder="New assignment..." class="p-2">
+                    <button type="submit" class="bg-white p-2 border-l">Add</button>
+                </div>
+            </form>
+        </section>
+    `,
+
+    data() {
+        return {
+            assigments: [
+                { id: 1, name: 'Finish project', complete: false },
+                { id: 2, name: 'Read chapter 4', complete: false },
+                { id: 3, name: 'Turn in homework', complete: false },
+            ],
+
+            newAssignment: ''
+        }
+    },
+
+    computed: {
+        filters() {
+            return {
+                inProgress: this.assigments.filter(assigment => ! assigment.complete),
+                completed: this.assigments.filter(assigment => assigment.complete)
+            }
+        }
+    },
+
+    methods: {
+        add() {
+            this.assigments.push({
+                name: this.newAssignment,
+                completed: false,
+                id: this.assigments.length + 1
+            });
+
+            this.newAssignment = '';
+        }
+    }
+}
